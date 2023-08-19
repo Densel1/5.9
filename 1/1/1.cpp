8378fae47c06f11b0fa118bdca0fe1753153d393
Igor Kastyrkin <tremen@yandex.ru>
2023-08-19 08:45:34 +0300

    replace errors

--- 1/1/1.cpp
+++ 1/1/1.cpp
@@ -3,11 +3,9 @@
 #include <vector>
 
 template<typename T>
-void move_vec(std::vector<T> v1, std::vector<T> &v2)
+void move_vec(std::vector<T> &v1, std::vector<T> &v2)
 {
-    std::vector<T> tmp(v1);
-    std::swap(tmp, v1);
-    v2.insert(v2.end(), tmp.begin(), tmp.end());
+      v2 = std::move(v1);
 }
 
 
@@ -15,7 +13,9 @@ int main()
 {
     std::vector <std::string> one = { "test_string1", "test_string2" };
     std::vector <std::string> two;
+
     move_vec(one, two);
+
     for (auto& c : two)
     {
         std::cout << c << " ";
